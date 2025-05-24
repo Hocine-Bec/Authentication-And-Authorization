c77f43f0b9c2e170f02ae63ef484c3f9d682b292
Hocine-Bec <Bechebil.Houcin@gmail.com>
2025-05-24 14:18:12 +0100

    feat: add EF Core configuration for TodoTask entity
    - Define primary key and column configurations
    - Configure Title, Description, and TaskStatus properties
    - Set up owned entity mapping for TimeSlot (DateRange)
    - Specify table name as "Tasks"

---
+++ BasicAuthentication/Data/Configs/TasksConfigurations.cs
@@ -0,0 +1,42 @@
+﻿using BasicAuthentication.Entities;
+using BasicAuthentication.Enums;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BasicAuthentication.Data.Configs
+{
+    public class TasksConfigurations : IEntityTypeConfiguration<TodoTask>
+    {
+        public void Configure(EntityTypeBuilder<TodoTask> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id)
+                .ValueGeneratedOnAdd();
+
+            builder.Property(x => x.Title)
+                .HasColumnName("Title")
+                .HasColumnType("VARCHAR")
+                .HasMaxLength(50)
+                .IsRequired();
+
+            builder.Property(x => x.Description)
+              .HasColumnName("Description")
+              .HasColumnType("VARCHAR")
+              .HasMaxLength(300)
+              .IsRequired(false);
+
+            builder.Property(x => x.TaskStatus)
+                .HasConversion<string>()
+                .HasColumnType("VARCHAR")
+                .HasMaxLength(30);
+
+            builder.OwnsOne(x => x.DateRange, ts =>
+            {
+                ts.Property(x => x.StartDate).HasColumnType("date").HasColumnName("StartDate").IsRequired();
+                ts.Property(x => x.EndDate).HasColumnType("date").HasColumnName("EndDate").IsRequired();
+            });
+
+            builder.ToTable("Tasks");
+        }
+    }
+}
