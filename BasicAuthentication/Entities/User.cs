b218824608f01aa82ea2a6c327f3c71e3e2bd66b
Hocine-Bec <Bechebil.Houcin@gmail.com>
2025-05-24 14:56:26 +0100

    refactor: set up Email property as nullable property

--- BasicAuthentication/Entities/User.cs
+++ BasicAuthentication/Entities/User.cs
@@ -5,7 +5,7 @@
         public int Id { get; set; }
         public required string Username { get; set; }
         public required string Password { get; set; }
-        public required string Email { get; set; }
+        public string? Email { get; set; }
 
         public ICollection<TodoTask> AssignedTasks{ get; set; } = new List<TodoTask>();
     }
