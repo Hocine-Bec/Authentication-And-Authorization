15f82258ed983338d2c16b8194d6e75595e3088e
Hocine-Bec <Bechebil.Houcin@gmail.com>
2025-05-24 14:19:13 +0100

    feat: add AppDbContext with TodoTasks DbSet
    - Include DbSet for TodoTask entity
    - Configure DbContext with options constructor
    - Apply entity configurations from assembly

---
+++ BasicAuthentication/Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using BasicAuthentication.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BasicAuthentication.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<TodoTask> TodoTasks { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+        }
+    }
+}
