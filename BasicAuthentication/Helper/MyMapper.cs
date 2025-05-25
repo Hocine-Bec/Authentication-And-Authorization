363c5439cae69239001942065298bcae7fe7677e
Hocine-Bec <Bechebil.Houcin@gmail.com>
2025-05-25 12:21:40 +0100

    feat: add CustomMapper for task operations
    - Implement MapUpdate extension method for TodoTask
    - Implement MapCreate method for new task initialization
    - Handle TaskRequest to TodoTask conversion with TimeSlot mapping

---
+++ BasicAuthentication/Helper/MyMapper.cs
@@ -0,0 +1,40 @@
+﻿using Azure.Core;
+using BasicAuthentication.DTOs;
+using BasicAuthentication.Entities;
+
+namespace BasicAuthentication.Helper
+{
+    public static class MyMapper
+    {
+        public static TodoTask MapUpdate(this TodoTask task, TaskRequest request)
+        {
+            task.Title = request.Title;
+            task.Description = request.Description;
+            task.TaskStatus = request.TaskStatus;
+            task.UserId = request.UserId;
+            task.DateRange = new TimeSlot
+            {
+                StartDate = request.StartDate,
+                EndDate = request.EndDate
+            };
+
+            return task;
+        }
+
+        public static TodoTask MapCreate(TaskRequest request)
+        {
+            return new TodoTask()
+            {
+                Title = request.Title,
+                Description = request.Description,
+                TaskStatus = request.TaskStatus,
+                UserId = request.UserId,
+                DateRange = new TimeSlot
+                {
+                    StartDate = request.StartDate,
+                    EndDate = request.EndDate
+                }
+            };
+        }
+    }
+}
