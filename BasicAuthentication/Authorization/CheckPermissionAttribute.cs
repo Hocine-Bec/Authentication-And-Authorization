cf13a1f23e07dc27eee8c38b8a04f26a0e0feb9a
Hocine-Bec <Bechebil.Houcin@gmail.com>
2025-05-28 12:03:09 +0100

    feat: add CheckPermission attribute for authorization
    - Create custom attribute for method-level permission checks
    - Takes Permission enum as constructor parameter
    - Configured for single use per method (AllowMultiple = false)

---
+++ BasicAuthentication/Authorization/CheckPermissionAttribute.cs
@@ -0,0 +1,10 @@
+﻿using BasicAuthentication.Enums;
+
+namespace BasicAuthentication.Authorization
+{
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
+    public class CheckPermissionAttribute(Permission permission) : Attribute
+    {
+        public Permission Permission { get; } = permission;
+    }
+}
