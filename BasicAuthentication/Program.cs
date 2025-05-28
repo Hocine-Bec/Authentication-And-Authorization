db5f2270bc8b34957014137a9b2f79ac1bfc0774
Hocine-Bec <Bechebil.Houcin@gmail.com>
2025-05-28 12:06:11 +0100

    feat: configure permission filter and testing setup
    - Register PermissionBasedAuthorizationFilter globally
    - Disable launchBrowser for Postman testing

--- BasicAuthentication/Program.cs
+++ BasicAuthentication/Program.cs
@@ -7,12 +7,17 @@ using BasicAuthentication.Authentication;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
+using BasicAuthentication.Authorization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(options =>
+{
+    options.Filters.Add<PermissionBasedAuthorizationFilter>();
+});
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 builder.Services.AddDbContext<AppDbContext>(options => 
