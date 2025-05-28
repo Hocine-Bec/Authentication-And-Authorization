e617f2caef7048fd258b6d15e4d2ccefcfe21bdc
Hocine-Bec <Bechebil.Houcin@gmail.com>
2025-05-28 16:46:10 +0100

    feat: implement PermissionAuthorizationHandler
    - Create handler for PermissionRequirements
    - Bypass checks for Admin role
    - Validate user permissions from database
    - Succeed requirement when authorized

---
+++ BasicAuthentication/Authorization/PermissionAuthorizationHandler.cs
@@ -0,0 +1,32 @@
+﻿using BasicAuthentication.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+
+namespace BasicAuthentication.Authorization
+{
+    public class PermissionAuthorizationHandler(AppDbContext context) : AuthorizationHandler<PermissionRequirements>
+    {
+        private readonly AppDbContext _context = context;
+
+        protected override async Task HandleRequirementAsync(AuthorizationHandlerContext context, PermissionRequirements requirement)
+        {
+            if(context.User.IsInRole("Admin"))
+            {
+                context.Succeed(requirement);
+                return;
+            }
+
+            if(!int.TryParse(context.User.FindFirst(ClaimTypes.NameIdentifier)?.Value, out int userId))
+                return;
+
+            var hasPermission = await _context.UserPermissions
+                .AnyAsync(x => x.UserId == userId && x.Permission == requirement.RequiredPermission);
+
+            if (!hasPermission)
+                return;
+
+            context.Succeed(requirement);
+        }
+    }
+}
