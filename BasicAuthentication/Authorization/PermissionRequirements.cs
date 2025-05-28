68ae72f778c5a22f1069ba22cc7c18c40bc249d9
Hocine-Bec <Bechebil.Houcin@gmail.com>
2025-05-28 16:40:52 +0100

    feat: add PermissionRequirements for policy-based auth
    - Implement IAuthorizationRequirement
    - Accept Permission enum in constructor
    - Store required permission as property

---
+++ BasicAuthentication/Authorization/PermissionRequirements.cs
@@ -0,0 +1,15 @@
+﻿using BasicAuthentication.Enums;
+using Microsoft.AspNetCore.Authorization;
+
+namespace BasicAuthentication.Authorization
+{
+    public class PermissionRequirements : IAuthorizationRequirement
+    {
+        public Permission RequiredPermission { get; }
+
+        public PermissionRequirements(Permission permission)
+        {
+            RequiredPermission = permission;
+        }
+    }
+}
