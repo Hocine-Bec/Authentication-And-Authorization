853dae95c11d16cf6f262bfb117eca9bd373fa3d
Hocine-Bec <Bechebil.Houcin@gmail.com>
2025-05-28 12:01:57 +0100

    feat: add UserPermission entity for authorization
    - Establish relationship between User and Permission
    - Include UserId and Permission properties

---
+++ BasicAuthentication/Entities/UserPermission.cs
@@ -0,0 +1,11 @@
+﻿using BasicAuthentication.Enums;
+
+namespace BasicAuthentication.Entities
+{
+    public class UserPermission
+    {
+        public int UserId { get; set; }
+        public User User { get; set; }
+        public Permission Permission { get; set; }
+    }
+}
