9fc5ba88d425764e26c770b7f651cac38e228657
Hocine-Bec <Bechebil.Houcin@gmail.com>
2025-05-28 12:02:49 +0100

    feat: configure UserPermission entity
    - Set composite key (UserId + Permission)
    - Define one-to-one relationship with User
    - Configure cascade delete behavior
    - Add DbSet to AppDbContext

--- BasicAuthentication/Data/AppDbContext.cs
+++ BasicAuthentication/Data/AppDbContext.cs
@@ -5,8 +5,10 @@ namespace BasicAuthentication.Data
 {
     public class AppDbContext : DbContext
     {
-        public virtual DbSet<TodoTask> Tasks { get; set; }
-        public virtual DbSet<User> Users { get; set; }
+        public DbSet<TodoTask> Tasks { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<UserPermission> UserPermissions { get; set; }
+
 
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
---
+++ BasicAuthentication/Data/Configs/UserPermissionsConfigurations.cs
@@ -0,0 +1,20 @@
+﻿using BasicAuthentication.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BasicAuthentication.Data.Configs
+{
+    public class UserPermissionsConfigurations : IEntityTypeConfiguration<UserPermission>
+    {
+        public void Configure(EntityTypeBuilder<UserPermission> builder)
+        {
+            builder.HasKey(x => new { x.UserId, x.Permission });
+
+            builder.HasOne(x => x.User)
+                .WithOne()
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.ToTable("UserPermissions");
+        }
+    }
+}
